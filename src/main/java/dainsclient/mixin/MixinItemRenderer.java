cef8a3ba9f2ef79bd38f9ce34d4c5fba058caf44
Dainternetdude <randomdude@kmamedia.com>
2020-11-10 15:52:45 -0600

    learning mixin

--- src/main/java/dainsclient/mixin/MixinItemRenderer.java
+++ src/main/java/dainsclient/mixin/MixinItemRenderer.java
@@ -1,67 +1,27 @@
 package dainsclient.mixin;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import sun.reflect.generics.tree.ReturnType;
+import net.minecraft.util.math.MathHelper;
 
 import java.util.Objects;
 
 @Mixin(ItemRenderer.class)
 public abstract class MixinItemRenderer {
-    @Shadow
-    public class ItemRenderer(Minecraft mc) {
-        this.mc = mc;
-    }
-    private final Minecraft mc;
-    private float prevEquippedProgressMainHand;
-    private float prevEquippedProgressOffHand;
-    private float equippedProgressMainHand;
-    private float equippedProgressOffHand;
-    private ItemStack itemStackMainHand;
-    private ItemStack itemStackOffHand;
-
-
-    @Shadow
-    public void updateEquippedItem() {
-        this.prevEquippedProgressMainHand = this.equippedProgressMainHand;
-        this.prevEquippedProgressOffHand = this.equippedProgressOffHand;
-        EntityPlayerSP entityplayersp = this.mc.player;
-        ItemStack itemstack = entityplayersp.getHeldItemMainhand();
-        ItemStack itemstack1 = entityplayersp.getHeldItemOffhand();
-
-        if (entityplayersp.isRowingBoat())
+    @Redirect(method = "updateEquippedItem",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemRenderer;clamp(F)F", ordinal = 2))
+    private static int clamp(int num, int min, int max) {
+        if(true)
         {
-            this.equippedProgressMainHand = MathHelper.clamp(this.equippedProgressMainHand - 0.4F, 0.0F, 1.0F);
-            this.equippedProgressOffHand = MathHelper.clamp(this.equippedProgressOffHand - 0.4F, 0.0F, 1.0F);
+            return 1;
         }
         else
         {
-            float f = entityplayersp.getCooledAttackStrength(1.0F);
-            //if(!this.mc.gameSettings.itemRefreshCooldown)
-            {
-                this.equippedProgressMainHand += MathHelper.clamp((float)(Objects.equals(this.itemStackMainHand, itemstack) ? 1 : 0) - this.equippedProgressMainHand, -0.4F, 0.4F);
-            }
-/*
-            else
-            {
-                this.equippedProgressMainHand += MathHelper.clamp((Objects.equals(this.itemStackMainHand, itemstack) ? f * f * f : 0.0F) - this.equippedProgressMainHand, -0.4F, 0.4F);
-            }*/
-            this.equippedProgressOffHand += MathHelper.clamp((float)(Objects.equals(this.itemStackOffHand, itemstack1) ? 1 : 0) - this.equippedProgressOffHand, -0.4F, 0.4F);
-
-        }
-
-        if (this.equippedProgressMainHand < 0.1F)
-        {
-            this.itemStackMainHand = itemstack;
-        }
-
-        if (this.equippedProgressOffHand < 0.1F)
-        {
-            this.itemStackOffHand = itemstack1;
+            return 0;
         }
     }
 }
