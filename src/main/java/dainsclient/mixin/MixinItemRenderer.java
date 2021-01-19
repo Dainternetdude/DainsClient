fed7b0df1ed0c7426ca4a94e83420e237cfad597
Deadly <modding.deadlymc@gmail.com>
2021-01-19 21:33:20 +0530

    Fix mod env

--- src/main/java/dainsclient/mixin/MixinItemRenderer.java
+++ src/main/java/dainsclient/mixin/MixinItemRenderer.java
@@ -1,20 +1,17 @@
 package dainsclient.mixin;
 
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import sun.reflect.generics.tree.ReturnType;
-import net.minecraft.util.math.MathHelper;
-
-import java.util.Objects;
 
 @Mixin(ItemRenderer.class)
-public abstract class MixinItemRenderer {
+public abstract class MixinItemRenderer
+{
     @Redirect(method = "updateEquippedItem()V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(FFF)F", ordinal = 3))
-    private static float clamp(float num, float min, float max) {
+    private float clamp(float num, float min, float max)
+    {
         if(true)
         {
             return 100;
